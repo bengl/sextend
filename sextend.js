55f08cf0721fb3edf92b4e1f7d75d264d5cfebb2
Bryan English <bryan@bryanenglish.com>
2014-12-07 16:13:15 -0800

    use prototype inheritance for static properties

--- sextend.js
+++ sextend.js
@@ -15,28 +15,9 @@
           configurable: true
         }
       });
-      // Set up _sextends variable, hidden.
-      Object.defineProperty(ctor, '_sextends', { enumerable: false, value: {} });
-      for (var s in superCtor) {
-        if (s === '_sextends' || !superCtor.hasOwnProperty(s)) return;
-        // Set up some accessors so we can get from super.
-        Object.defineProperty(ctor, s, {
-          enumerable: true,
-          get: function() {
-            // First search our _sextends. If not there, try super's.
-            // If not there, try property on super.
-            if (this._sextends[s]) {
-              return this._sextends[s];
-            } else {
-              return superCtor._sextends ? superCtor._sextends[s] : superCtor[s];
-            }
-          },
-          set: function(thing) {
-            // Override's super's version. Just like prototype. :)
-            this._sextends[s] = thing;
-          }
-        });
-      }
+
+      // Static properties (same as ES6)
+      ctor.__proto__ = superCtor;
     }
 
     if (arguments.length > 2) {
