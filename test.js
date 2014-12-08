7a4011ff7fc4b9f7e4917f9f0efafc88031b3c84
Bryan English <bryan@bryanenglish.com>
2014-12-07 16:33:28 -0800

    tape tests

--- test.js
+++ test.js
@@ -1,47 +1,52 @@
 var sextend = require('./sextend'),
-    assert = require('assert');
+    tape = require('tape');
 
-function A(){}
-A.prototype.a = 12345;
-A.q = 54321;
+tape('sextend', function (t) {
+  t.plan(13);
 
-function B(){}
-sextend(B, A);
+  function A(){}
+  A.prototype.a = 12345;
+  A.q = 54321;
 
-assert(B.super_ === A);
+  function B(){}
+  sextend(B, A);
 
-assert(B.q === 54321);
-B.q = "go";
-assert(B.q === "go");
-assert(A.q === 54321);
-assert(B.prototype.a === 12345);
+  t.equal(B.super_, A);
 
-function C(){}
-sextend(C, B);
+  t.equal(B.q, 54321);
+  B.q = "go";
+  t.equal(B.q, "go");
+  t.equal(A.q, 54321);
+  t.equal(B.prototype.a, 12345);
 
-assert(C.q === "go");
-assert(C.prototype.a === 12345);
+  function C(){}
+  sextend(C, B);
 
-sextend(C); // should not throw
-sextend(C, null); //should not throw
+  t.equal(C.q, "go");
+  t.equal(C.prototype.a, 12345);
 
-function D() {}
-D.s = 72;
-var mixin1 = function(){
-  this.t = 27;
-  this.prototype.u = 28;
-};
+  sextend(C); // should not throw
+  sextend(C, null); //should not throw
 
-var mixin2 = function() {
-  this.v = 29;
-};
+  function D() {}
+  D.s = 72;
+  var mixin1 = function(){
+    this.t = 27;
+    this.prototype.u = 28;
+  };
 
-sextend(D, C, mixin1, mixin2);
-assert(D.s === 72);
-assert(D.t === 27);
-assert((new D()).u === 28);
-assert((new D()).a === 12345);
-assert(D.v === 29);
+  var mixin2 = function() {
+    this.v = 29;
+  };
 
-sextend(B, null, mixin2);
-assert(B.v === 29);
+  sextend(D, C, mixin1, mixin2);
+  t.equal(D.s, 72);
+  t.equal(D.t, 27);
+  t.equal((new D()).u, 28);
+  t.equal((new D()).a, 12345);
+  t.equal(D.v, 29);
+
+  sextend(B, null, mixin2);
+  t.equal(B.v, 29);
+
+});
