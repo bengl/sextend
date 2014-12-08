613495d12cddeb18ddd044ac61a29a958b503c0d
Bryan English <bryan@bryanenglish.com>
2014-12-07 16:18:39 -0800

    commonjs only. web? use broswerify.

--- sextend.js
+++ sextend.js
@@ -1,37 +1,24 @@
-(function () {
-
-  var root = this; 
-
-  var sextend = function(ctor, superCtor) {
-
-    if (superCtor) {
-      // Normal node inheritance (util.inherits)
-      ctor.super_ = superCtor;
-      ctor.prototype = Object.create(superCtor.prototype, {
-        constructor: {
-          value: ctor,
-          enumerable: false,
-          writable: true,
-          configurable: true
-        }
-      });
-
-      // Static properties (same as ES6)
-      ctor.__proto__ = superCtor;
-    }
-
-    if (arguments.length > 2) {
-      var mixins = [].slice.call(arguments, 2);
-      for (var i=0; i < mixins.length; ++i) {
-        mixins[i].call(ctor);
+module.exports = function sextend (ctor, superCtor) {
+  if (superCtor) {
+    // Normal node inheritance (util.inherits)
+    ctor.super_ = superCtor;
+    ctor.prototype = Object.create(superCtor.prototype, {
+      constructor: {
+        value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
       }
-    }
+    });
+
+    // Static properties (same as ES6)
+    ctor.__proto__ = superCtor;
   }
 
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = sextend;
-    root.sextend = sextend;
-  } else {
-    root.sextend = sextend;
+  if (arguments.length > 2) {
+    var mixins = [].slice.call(arguments, 2);
+    for (var i=0; i < mixins.length; ++i) {
+      mixins[i].call(ctor);
+    }
   }
-})();
+};
